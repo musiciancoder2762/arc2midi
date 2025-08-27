9225f8aeeffcdf5a3c57740a84fe398f76b63d73
musiciancoder2762 <ericliuyangchile@gmail.com>
2025-08-27 15:28:53 -0400

    temporarily skip percussion track to prevent crashing

--- arc2midi.ts
+++ arc2midi.ts
@@ -18,7 +18,7 @@ namespace arc2MIDI {
         if (val === 0) {
             return 1;
         } else {
-            return Math.floor(Math.log(Math.abs(val)))/Math.log(2);
+            return Math.floor(Math.log(Math.abs(val))) / Math.log(2);
         }
     };
 
@@ -27,13 +27,13 @@ namespace arc2MIDI {
         let trackArray: Buffer[] = [];
         for (let trk = 0; trk < trackAmount; trk++) {
             trackArray.push(file.slice(byteInd, file.getNumber(NumberFormat.UInt16LE, byteInd + 32) + 34));
-            byteInd += file.getNumber(NumberFormat.UInt16LE,byteInd+32)+34;
+            byteInd += file.getNumber(NumberFormat.UInt16LE, byteInd + 32) + 34;
         }
         return trackArray;
     };
 
     function convertArcNotetoMIDINote(note: number, octave: number) {
-        return (note&63) + (12 * octave) - 1;
+        return (note & 63) + (12 * octave) - 1;
     };
 
     function extractEvents(track: Buffer, channelNo: number) {
@@ -44,58 +44,61 @@ namespace arc2MIDI {
         let deltaInd: number = 0;
         let byteIndex: number = 0;
         let noteAmount: number = 1;
-        if (channelNo>15) {channelNo = 15};
-        if (channelNo===9) {channelNo = 10}; // channel 0xn9 (channel 10) is reserved for percussion
+        if (channelNo > 15) { channelNo = 15 };
+        if (channelNo === 9) { channelNo = 10 }; // channel 0xn9 (channel 10) is reserved for percussion
         if (noteVelocity > 0x7f) { noteVelocity = 0x7f; };
-        if (track[1]===1) {
+        if (track[1] === 1) {
+            isPercussion = true;
             return [[-1]];
+        } else {
+            isPercussion = false;
         } // detect if track is percussion/drum, then skip this track to prevent it from crashing (temp)
         while (byteIndex < cleanTrk.length) {
             noteAmount = 0;
-            while (noteAmount < cleanTrk.getUint8(byteIndex+4)) {
+            while (noteAmount < cleanTrk.getUint8(byteIndex + 4)) {
                 events.push([cleanTrk.getNumber(NumberFormat.UInt16LE, 0 + byteIndex) * (tick480 ? tickConstant : 1), 0x90 + channelNo, convertArcNotetoMIDINote(cleanTrk.getUint8(noteAmount + byteIndex + 5), instrumentOctave), noteVelocity]);
                 events.push([cleanTrk.getNumber(NumberFormat.UInt16LE, 2 + byteIndex) * (tick480 ? tickConstant : 1), 0x80 + channelNo, convertArcNotetoMIDINote(cleanTrk.getUint8(noteAmount + byteIndex + 5), instrumentOctave), 0]);
                 noteAmount++;
             }
             byteIndex += (5 + cleanTrk.getUint8(byteIndex + 4));
         }
-        events.sort((a,b) => {
+        events.sort((a, b) => {
             return a[0] - b[0];
         });
         deltaEvents.push(events[0][0]);
-        while (deltaInd < events.length-1) {
-            deltaEvents.push(events[deltaInd + 1][0] - events[deltaInd ][0]);
+        while (deltaInd < events.length - 1) {
+            deltaEvents.push(events[deltaInd + 1][0] - events[deltaInd][0]);
             deltaInd++;
         }
         deltaInd = 0;
         while (deltaInd < events.length) {
-            events[deltaInd][0]=deltaEvents[deltaInd];
+            events[deltaInd][0] = deltaEvents[deltaInd];
             deltaInd++;
         }
         return events;
     };
 
     function generateTrack(events: number[][], trackNumber?: number,) {
-        if (trackNumber === undefined) {trackNumber = 0;};
+        if (trackNumber === undefined) { trackNumber = 0; };
         let trackBuffer: Buffer = Buffer.create(23);
         let eventBuffer: Buffer = Buffer.create(0);
-        trackBuffer.write(0,Buffer.fromUTF8("MTrk")); // midi header
-        trackBuffer.write(8,Buffer.fromHex("00ff0308")); // declare track name
+        trackBuffer.write(0, Buffer.fromUTF8("MTrk")); // midi header
+        trackBuffer.write(8, Buffer.fromHex("00ff0308")); // declare track name
         trackBuffer.write(12, Buffer.fromUTF8(`TRACK ${trackNumber < 10 ? `0${trackNumber}` : trackNumber}`)); // set track name
-        trackBuffer.write(20,Buffer.fromHex("00c000")); // set instrument to piano
-        if (events === [[-1]]) {
+        trackBuffer.write(20, Buffer.fromHex("00c000")); // set instrument to piano
+        if (isPercussion) {
             return Buffer.fromHex("4D54726B0000001200FF030A5045524320545241434B00FF2F00");
         } // detect if percussion, then return empty track;
-        events.forEach(function(value,index) {
-            int2vlq(value[0]).forEach(function(v,i) {
+        events.forEach(function (value, index) {
+            int2vlq(value[0]).forEach(function (v, i) {
                 eventBuffer = eventBuffer.concat(Buffer.fromArray([v]));
             });
-            eventBuffer = eventBuffer.concat(Buffer.fromArray([value[1],value[2],value[3]]));
+            eventBuffer = eventBuffer.concat(Buffer.fromArray([value[1], value[2], value[3]]));
             trackBuffer = trackBuffer.concat(eventBuffer);
             eventBuffer = Buffer.create(0);
         });
         trackBuffer = trackBuffer.concat(Buffer.fromHex("00ff2f00"));
-        trackBuffer.setNumber(NumberFormat.UInt32BE,4,trackBuffer.length-8);
+        trackBuffer.setNumber(NumberFormat.UInt32BE, 4, trackBuffer.length - 8);
         return trackBuffer;
     };
 
@@ -103,23 +106,23 @@ namespace arc2MIDI {
         let finalMIDI: Buffer = Buffer.create(14);
         let starterTrack: Buffer = Buffer.create(42);
 
-        finalMIDI.write(0,Buffer.fromUTF8("MThd"));
-        finalMIDI.setUint8(7,6);
-        finalMIDI.setUint8(9,1);
-        finalMIDI.setNumber(NumberFormat.UInt16BE,10,trackAmount+1);
-        finalMIDI.setNumber(NumberFormat.UInt16BE,12,tick480?480:tickRate);
-        starterTrack.write(0,Buffer.fromUTF8("MTrk"));
-        starterTrack.setUint8(7,34);
-        starterTrack.write(8,Buffer.fromHex("00ff030b"));
-        starterTrack.write(12,Buffer.fromUTF8("TEMPO TRACK"));
-        starterTrack.write(23,Buffer.fromHex("00ff5804aa041808"));
-        starterTrack.setUint8(27,timeSignature);
-        starterTrack.write(31,Buffer.fromHex("00ff5103aabbcc"));
-        starterTrack.setNumber(NumberFormat.UInt32BE,34,(60/bpm)*1000000);
-        starterTrack.setUint8(34,3);
-        starterTrack.write(38,Buffer.fromHex("00ff2f00"));
+        finalMIDI.write(0, Buffer.fromUTF8("MThd"));
+        finalMIDI.setUint8(7, 6);
+        finalMIDI.setUint8(9, 1);
+        finalMIDI.setNumber(NumberFormat.UInt16BE, 10, trackAmount + 1);
+        finalMIDI.setNumber(NumberFormat.UInt16BE, 12, tick480 ? 480 : tickRate);
+        starterTrack.write(0, Buffer.fromUTF8("MTrk"));
+        starterTrack.setUint8(7, 34);
+        starterTrack.write(8, Buffer.fromHex("00ff030b"));
+        starterTrack.write(12, Buffer.fromUTF8("TEMPO TRACK"));
+        starterTrack.write(23, Buffer.fromHex("00ff5804aa041808"));
+        starterTrack.setUint8(27, timeSignature);
+        starterTrack.write(31, Buffer.fromHex("00ff5103aabbcc"));
+        starterTrack.setNumber(NumberFormat.UInt32BE, 34, (60 / bpm) * 1000000);
+        starterTrack.setUint8(34, 3);
+        starterTrack.write(38, Buffer.fromHex("00ff2f00"));
         finalMIDI = finalMIDI.concat(starterTrack);
-        trackArrays.forEach(function(track,trackIndex) {
+        trackArrays.forEach(function (track, trackIndex) {
             finalMIDI = finalMIDI.concat(track);
         });
         return finalMIDI;
@@ -132,20 +135,22 @@ namespace arc2MIDI {
         noteVelocity = 90;
         timeSignature = file.getUint8(3);
         tickConstant = tick480 ? 480 / file.getUint8(4) : 1;
-        tickRate = tick480?480:file.getUint8(4)/4;
+        tickRate = tick480 ? 480 : file.getUint8(4) / 4;
         trackAmount = file.getUint8(6);
         let trackData: Buffer[] = getTrackData(file);
-        trackData.forEach(function(v,i) {
-            trackBufferArray.push(generateTrack(extractEvents(v,i),i)); // line of death. guaranteed arcade crash
+        trackData.forEach(function (v, i) {
+            trackBufferArray.push(generateTrack(extractEvents(v, i), i)); // line of death. guaranteed arcade crash
         });
-        console.log(b64output ? assembleMIDIFile(trackBufferArray).toBase64() : assembleMIDIFile(trackBufferArray).toHex());
+        const midiFile: Buffer = assembleMIDIFile(trackBufferArray);
+        console.log(midiFile);
     };
     export let tick480: boolean = true; // set tick rate of midi file to 480 ticks/quarter note
     let bpm: uint16;
+    let isPercussion: boolean = false;
     let noteVelocity: uint8;
     let timeSignature: uint8;
     let tickConstant: number;
     let tickRate: uint16;
     let trackAmount: uint8;
-    export let b64output: boolean = true;
+    export let b64output: boolean = false;
 }
